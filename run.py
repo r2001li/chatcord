dfc8871b774c1e0e3aa21ce06c38be6d16e75390
r2001li <ruiang2001@gmail.com>
2025-03-31 12:34:13 -0400

    Implemented chat session expiration

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import requests
+import datetime
 import json
 import tomllib
 from ollama import ChatResponse, Client
@@ -8,7 +8,6 @@ import discord
 from discord.ext import commands
 
 from dotenv import load_dotenv
-from asyncio import timeout
 
 with open("config/config.toml", 'rb') as f:
     config_data = tomllib.load(f)
@@ -38,6 +37,9 @@ client = discord.Client(intents=intents)
 
 # Initialize Ollama client
 ollama_client = Client(host=HOST_URL)
+
+# Initialize listeners
+listening_channels = {}
 
 def generate_response(prompt):
     chat_history.append({
@@ -69,30 +71,55 @@ def generate_response(prompt):
 
     return assistant_message
 
+async def process_message(message: discord.Message):
+    # Format prompt
+    prompt = message.clean_content
+    prompt = f"{message.author.display_name} messages you: {prompt}"
+
+    # Send generated response
+    try:
+        async with message.channel.typing():
+            response = generate_response(prompt)
+            await message.channel.send(response)
+    except discord.errors.Forbidden:
+        print(f"Error: Bot does not have permission to type in {message.channel.name}")
+    
+    return
+
+
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
 
 @client.event
-async def on_message(message):
+async def on_message(message: discord.Message):
+    # Ignore messages from self or bots
     if message.author == client.user:
         return
 
-    if message.author.bot == True:
+    if message.author.bot:
         return
 
-    if client.user.mentioned_in(message):
-        prompt = message.clean_content
-        prompt = f"{message.author.display_name} messages you: {prompt}"
+    # Clean up any expired session
+    now = datetime.datetime.utcnow()
+    channel_id = message.channel.id
 
-        try:
-            async with message.channel.typing():
-                if prompt:
-                    response = generate_response(prompt)
-                    await message.channel.send(response)
-        except discord.errors.Forbidden:
-            print(f"Error: Bot does not have permission to type in {message.channel.name}")
-            return
+    if channel_id in listening_channels:
+        if now > listening_channels[channel_id]:
+            del listening_channels[channel_id]
+            print("Session expired for channel: ", channel_id)
+
+    # When mentioned, answer prompt and start listening
+    if client.user in message.mentions:
+        listening_channels[channel_id] = now + datetime.timedelta(minutes=2)
+        print("Starting session for channel: ", channel_id)
+        return await process_message(message)
+
+    # If not mentioned, answer prompt only if channel is active
+    if channel_id in listening_channels:
+        listening_channels[channel_id] = now + datetime.timedelta(minutes=2)
+        print("Extending session for channel: ", channel_id)
+        return await process_message(message)
 
 load_dotenv()
 client.run(os.getenv("TOKEN"))
